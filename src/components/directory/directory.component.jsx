22958d03941948bab594444cc7b948a91bc748b4
Sriram <43503475+ScionOfIksvaku@users.noreply.github.com>
2019-08-06 17:29:11 -0500

    moved directory and shop data into their own reducers, created respective selectors updated corresponding components with redux flow

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,66 +1,23 @@
-import React, {Component} from 'react';
-import MenuItem from '../menu-item/menu-item.component';
-import './directory.styles.scss'
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-class Directory extends Component {
-    constructor(){
-        super();
+import { selectDirectorySections } from "../../redux/directory/directory.selector";
 
-        this.state = {
-          sections: [
-            {
-              title: "Hats",
-              imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-              id: 1,
-              linkUrl: "shop/hats"
-            },
-            {
-              title: "Jackets",
-              imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-              id: 2,
-              linkUrl: "shop/jackets"
-            },
-            {
-              title: "Sneakers",
-              imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-              id: 3,
-              linkUrl: "shop/sneakers"
-            },
-            {
-              title: "Womens",
-              imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-              size: "large",
-              id: 4,
-              linkUrl: "shop/womens"
-            },
-            {
-              title: "Mens",
-              imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-              size: "large",
-              id: 5,
-              linkUrl: "shop/mens"
-            }
-          ]
-        };
-    }
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ title, imageUrl, id, size }) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      ))}
+    </div>
+  );
+};
 
-    render(){
-        return(
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({title,imageUrl,id, size}) => (
-                        <MenuItem 
-                        key={id} 
-                        title={title}
-                        imageUrl={imageUrl}
-                        size={size}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-                        />
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,11 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
+import { persistStore } from "redux-persist";
+
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
